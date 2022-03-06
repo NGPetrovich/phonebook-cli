3d8ebc655731130af45cef4124e27949451e5a47
Nikita Goncharenko <nikita.122495@gmail.com>
2022-03-06 13:09:08 +0100

    big success: it works as expected. Project completed!

--- mongo.js
+++ mongo.js
@@ -6,14 +6,35 @@ mongoose.connect(url)
 
 const Contact = mongoose.model('Contact', {
   name: String,
-  number: Number
+  number: String
 })
 
-Contact
-  .find({})
-  .then(contacts => {
-    contacts.forEach(contact => {
-      console.log(contact)
-    })
-    mongoose.connection.close()
+const { argv } = process;
+const [, , inputName, inputNumber] = argv;
+
+const contact = new Contact({
+  name: inputName,
+  number: inputNumber
 })
+
+// console.log(contact.name)
+// console.log(contact.number)
+
+if (!contact.name && !contact.number) {
+  Contact
+    .find({})
+    .then(contacts => {
+      console.log("Contacts:")
+      contacts.forEach(contact => {
+        console.log(contact.name, contact.number)
+      })
+      mongoose.connection.close()
+    })
+} else {
+  contact
+    .save()
+    .then(result => {
+      console.log(`adding person ${contact.name} number ${contact.number} to the directory`);
+      mongoose.connection.close()
+    })
+}
